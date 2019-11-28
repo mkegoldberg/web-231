249361a4e272b2af104038824d712f210a524946
Mike Goldberg <mkegoldberg@yahoo.com>
2019-11-27 22:20:42 -0800

    adding assignment 1.4

---
+++ week-1/goldberg-assignment-1.4.js
@@ -0,0 +1,20 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Mike Goldberg
+; Date: 11/26/2019
+; Modified By: Mike Goldberg
+; Description: Examples of primitive types
+;===========================================
+*/
+
+var firstName = "Mike";
+var lastName = "Goldberg";
+var age = "41";
+var street = "Poinsettia Lane";
+var city = "San Diego";
+var zipCode = "92011";
+
+console.log(firstName, lastName, age, street, city, zipCode);
+
+
