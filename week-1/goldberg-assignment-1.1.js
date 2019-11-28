4052cb92fac880a7352bd500eb6bcf731ebe93a8
Mike Goldberg <mkegoldberg@yahoo.com>
2019-11-27 21:32:44 -0800

    adding assignment 1.1

---
+++ week-1/goldberg-assignment-1.1.js
@@ -0,0 +1,25 @@
+/*
+============================================
+; Title: Assignment 1.1
+; Author: Mike Goldberg
+; Date: 11/26/2019
+; Modified By: Mike Goldberg
+; Description: This snippet contains errors for 
+; to be audited by other students
+;===========================================
+*/
+
+/*
+  Expected Output
+
+  Mike's current mood
+*/
+
+// variable declarations
+var firstName = "Mike";
+var timeOfDay === 23;
+var canDrinkAlcohol = timeOfDay >= 5;
+var result = "canDrinkAlcohol" ? `${firstName} is in a good mood!` : `${firstName} is so crabby`;
+
+// output
+console.log(result);
