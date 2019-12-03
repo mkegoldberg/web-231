f45d71129629d2d70b79c2caad5174f04ade905b
Mike Goldberg <mkegoldberg@yahoo.com>
2019-12-02 22:49:21 -0800

    Pushing Header file and test

---
+++ week-2/goldberg-header-test.js
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Mike Goldberg
+; Date:   02 December 2019
+; Description: Displays a formatted header
+;===========================================
+*/
+
+// Require statement that imports the header.js file from my root directory.
+const header = require('../goldberg-header.js');
+
+console.log(header.display("Mike", "Goldberg", "Assignment 2.2"));
