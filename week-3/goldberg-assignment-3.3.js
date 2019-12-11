7998ba8911b6959ece1b6389468f0a713329504b
Mike Goldberg <mkegoldberg@yahoo.com>
2019-12-10 21:20:09 -0800

    adding assignment 3.3

---
+++ week-3/goldberg-assignment-3.3.js
@@ -0,0 +1,53 @@
+/*
+============================================
+; Title: Assignment 3.3
+; Author: Mike Goldberg
+; Date: 12/10/2019
+; Modified By: Mike Goldberg
+; Description: Multiway Branch Statements
+;===========================================
+*/
+// Require statement that imports the header.js file from my root directory.
+const header = require('../goldberg-header.js');
+
+
+// start program
+
+/*
+  Expected output:
+
+  FirstName LastName
+  Exercise 3.3
+  Today's Date
+
+  // Expected output
+  The enter key was pressed
+*/
+
+// variable declaration
+let eventKeyCode = 13
+
+// functions
+switch (eventKeyCode) {
+  case 13:
+    console.log("The enter key was pressed.");
+    break;
+  case 16:
+    console.log("The shift key was pressed.");
+    break;
+  case 32:
+    console.log("The spacebar key was pressed.");
+    break;
+  case 8:
+    console.log("The backspace / delete key was pressed.");
+    break;
+  default:
+    console.log("Unrecognized key.")
+    break;
+}
+
+// output
+console.log(header.display("Mike", "Goldberg", "assignment 3.2"), "\n");
+
+// end program
+
