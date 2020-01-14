176b7b47a792f695c726b00df1b82771ecbbe685
Mike Goldberg <mkegoldberg@yahoo.com>
2020-01-13 22:06:11 -0800

    adding 6.3

---
+++ week-6/goldberg-assignment-6.3.js
@@ -0,0 +1,40 @@
+/*
+============================================
+; Title: Assignment 6.3
+; Author: Mike Goldberg
+; Date: 01/13/2020
+; Modified By: Mike Goldberg
+; Description: Exception Handling
+;===========================================
+*/
+// Require statement that imports the header.js file from my root directory.
+const header = require('../goldberg-header.js');
+
+// start program
+
+/*
+    expected output:
+
+    id: 231, name: Product Development, requester: Ricky Bobby
+*/
+
+// variable declaration
+var ticket = {
+  id: 231,
+  name: "Product Development",
+  requester: "Ricky Bobby"
+};
+var logArray = [];
+
+// output
+console.log(header.display("Mike", "Goldberg", "assignment 6.2"), "\n");
+
+// loop through object
+for (var item in ticket) {
+  // push each key/val pair to an array
+  logArray.push(item + ": " + ticket[item]);
+}
+// combine array into one long string and print
+console.log(logArray.join(", "));
+
+//  end program
